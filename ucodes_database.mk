30b2f3262b5c8ebf8841e5f5a757c5e4af8a67c0
Fanatic-64 <fanatic.64_emu64@yahoo.com>
2023-09-14 18:58:11 -0600

    Change microcode names to more closely match the official ones

--- ucodes_database.mk
+++ ucodes_database.mk
@@ -1,4 +1,4 @@
-NAME := F3DEX2_2.04
+NAME := gspF3DEX2.04.fifo
 DESCRIPTION := Goemon's Great Adventure
 ID_STR := RSP Gfx ucode F3DEX       fifo 2.04  Yoshitaka Yasumoto 1998 Nintendo.
 MD5_CODE := bfa243516576e87151b180ec9d34b59d
@@ -14,7 +14,7 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_NoN_2.04
+NAME := gspF3DEX2.04.NoN.fifo
 DESCRIPTION := California Speed
 ID_STR := RSP Gfx ucode F3DEX.NoN   fifo 2.04  Yoshitaka Yasumoto 1998 Nintendo.
 MD5_CODE := 08c9cfc0fe181b79cb40b32c5ab0672a
@@ -31,7 +31,7 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_2.04H
+NAME := gspF3DEX2.04H.fifo
 DESCRIPTION := Kirby 64, Smash 64
 ID_STR := RSP Gfx ucode F3DEX       fifo 2.04H Yoshitaka Yasumoto 1998 Nintendo.
 MD5_CODE := d3a58568fa7cf042de370912a47c3b5f
@@ -48,8 +48,8 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_NoN_2.04H
-DESCRIPTION := Not in retail games; NoN added to F3DEX2_2.04H
+NAME := gspF3DEX2.04H.NoN.fifo
+DESCRIPTION := Not in retail games; NoN added to F3DEX2.04H.fifo
 ID_STR := RSP Gfx ucode F3DEX.NoN   fifo 2.04H Yoshitaka Yasumoto 1998 Nintendo.
 OPTIONS := \
   CFG_NoN \
@@ -64,7 +64,7 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_2.05
+NAME := gspF3DEX2.05.fifo
 DESCRIPTION := Snowboard Kids 2
 ID_STR := RSP Gfx ucode F3DEX       fifo 2.05  Yoshitaka Yasumoto 1998 Nintendo.
 MD5_CODE := 84f4cdfe40c2bd707f81263c9fd1c213
@@ -75,7 +75,7 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_NoN_2.05
+NAME := gspF3DEX2.05.NoN.fifo
 DESCRIPTION := The New Tetris
 ID_STR := RSP Gfx ucode F3DEX.NoN   fifo 2.05  Yoshitaka Yasumoto 1998 Nintendo.
 MD5_CODE := 461a39ee1a3e780abd501d7565158407
@@ -87,7 +87,7 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_2.06
+NAME := gspF3DEX2.06.fifo
 DESCRIPTION := Pokemon Stadium
 ID_STR := RSP Gfx ucode F3DEX       fifo 2.06  Yoshitaka Yasumoto 1998 Nintendo.
 MD5_CODE := 93f6660e434d30ab1d88e94aa2a2ffe2
@@ -97,7 +97,7 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_NoN_2.06
+NAME := gspF3DEX2.06.NoN.fifo
 DESCRIPTION := Mario Party
 ID_STR := RSP Gfx ucode F3DEX.NoN   fifo 2.06  Yoshitaka Yasumoto 1998 Nintendo.
 MD5_CODE := a8cfd5805ec35d7056f244f700a6328c
@@ -108,7 +108,7 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_NoN_2.06_XBUS
+NAME := gspF3DEX2.06.NoN.xbus
 DESCRIPTION := Command & Conquer
 ID_STR := RSP Gfx ucode F3DEX.NoN   xbus 2.06  Yoshitaka Yasumoto 1998 Nintendo.
 MD5_CODE := 820b2409bece0c5598d65a0184a2f7f3
@@ -120,7 +120,7 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_2.07
+NAME := gspF3DEX2.07.fifo
 DESCRIPTION := Rocket: Robot on Wheels
 ID_STR := RSP Gfx ucode F3DEX       fifo 2.07  Yoshitaka Yasumoto 1998 Nintendo.
 MD5_CODE := 1523b8e38a9eae698b48909a0c0c0279
@@ -129,7 +129,7 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_2.07_XBUS
+NAME := gspF3DEX2.07.xbus
 DESCRIPTION := Lode Runner 3-D
 ID_STR := RSP Gfx ucode F3DEX       xbus 2.07  Yoshitaka Yasumoto 1998 Nintendo.
 MD5_CODE := b882f402e115ffaf05a9ee44f354c441
@@ -139,7 +139,7 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_NoN_2.07
+NAME := gspF3DEX2.07.NoN.fifo
 DESCRIPTION := Tom Clany's Rainbow Six
 ID_STR := RSP Gfx ucode F3DEX.NoN   fifo 2.07  Yoshitaka Yasumoto 1998 Nintendo.
 MD5_CODE := 59d57894db276f7c7a2f399dfbc4e5f3
@@ -149,7 +149,7 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_2.08
+NAME := gspF3DEX2.08.fifo
 DESCRIPTION := Banjo-Tooie
 ID_STR := RSP Gfx ucode F3DEX       fifo 2.08  Yoshitaka Yasumoto 1999 Nintendo.
 MD5_CODE := 6ccf5fc392e440fb23bc7d7f7d71047c
@@ -157,7 +157,7 @@ MD5_DATA := 3a3a406acb4295d33fa6e918dd3a7ae4
 OPTIONS := 
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_2.08_XBUS
+NAME := gspF3DEX2.08.xbus
 DESCRIPTION := Power Rangers
 ID_STR := RSP Gfx ucode F3DEX       xbus 2.08  Yoshitaka Yasumoto 1999 Nintendo.
 MD5_CODE := 38cbd8ef2cd168141347047cf7ec4fba
@@ -166,7 +166,7 @@ OPTIONS := \
   CFG_XBUS
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_NoN_2.08
+NAME := gspF3DEX2.08.NoN.fifo
 DESCRIPTION := Mario Party 2, Mario Party 3
 ID_STR := RSP Gfx ucode F3DEX.NoN   fifo 2.08  Yoshitaka Yasumoto 1999 Nintendo.
 MD5_CODE := b5c366b55a032f232aa309cda21be3d7
@@ -175,7 +175,7 @@ OPTIONS := \
   CFG_NoN
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_NoN_2.08_XBUS
+NAME := gspF3DEX2.08.NoN.xbus
 DESCRIPTION := Excitebike 64
 ID_STR := RSP Gfx ucode F3DEX.NoN   xbus 2.08  Yoshitaka Yasumoto 1999 Nintendo.
 MD5_CODE := b1954a4affaaf16d86a6924b7a40627c
@@ -185,14 +185,14 @@ OPTIONS := \
   CFG_XBUS
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_2.08H
-DESCRIPTION := Not in retail games; nearclipping added to F3DEX2_NoN_2.08H
+NAME := gspF3DEX2.08H.fifo
+DESCRIPTION := Not in retail games; nearclipping added to F3DEX2.08H.NoN.fifo
 ID_STR := RSP Gfx ucode F3DEX       fifo 2.08H Yoshitaka Yasumoto 1999 Nintendo.
 OPTIONS := \
   CFG_G_SPECIAL_1_IS_RECALC_MVP
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_NoN_2.08H
+NAME := gspF3DEX2.08H.NoN.fifo
 DESCRIPTION := Pokemon Snap
 ID_STR := RSP Gfx ucode F3DEX.NoN   fifo 2.08H Yoshitaka Yasumoto 1999 Nintendo.
 MD5_CODE := 7a1e9c51f778c4fe85c695c385085fd3
@@ -202,7 +202,7 @@ OPTIONS := \
   CFG_NoN
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_2.08PL
+NAME := gspF3DEX2.08_PosLight.fifo
 DESCRIPTION := Paper Mario
 ID_STR := RSP Gfx ucode F3DEX       fifo 2.08  Yoshitaka Yasumoto/Kawasedo 1999.
 MD5_CODE := 6a5117e62e51d87020fb81dc493efcb6
@@ -211,16 +211,16 @@ OPTIONS := \
   CFG_POINT_LIGHTING
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_NoN_2.08PL
-DESCRIPTION := Not in retail games; NoN added to F3DEX2_2.08PL
+NAME := gspF3DEX2.08.NoN_PosLight.fifo
+DESCRIPTION := Not in retail games; NoN added to F3DEX2.08_PosLight.fifo
 ID_STR := RSP Gfx ucode F3DEX.NoN   fifo 2.08  Yoshitaka Yasumoto/Kawasedo 1999.
 OPTIONS := \
   CFG_NoN \
   CFG_POINT_LIGHTING
 $(eval $(call ucode_rule))
 
-NAME := F3DEX2_NoN_2.08HPL
-DESCRIPTION := Not in retail games; PL added to F3DEX2_NoN_2.08H
+NAME := gspF3DEX2.08H.NoN_PosLight.fifo
+DESCRIPTION := Not in retail games; PosLight added to F3DEX2.08H.NoN.fifo
 ID_STR := RSP Gfx ucode F3DEX.NoN   fifo 2.08H Yoshitaka Yasumoto/Kawasedo 1999.
 OPTIONS := \
   CFG_G_SPECIAL_1_IS_RECALC_MVP \
@@ -228,8 +228,8 @@ OPTIONS := \
   CFG_POINT_LIGHTING
 $(eval $(call ucode_rule))
 
-NAME := F3DZEX_2.06H
-DESCRIPTION := Not in retail games; nearclipping added to F3DZEX_NoN_2.06H
+NAME := gspF3DZEX2.06H.fifo
+DESCRIPTION := Not in retail games; nearclipping added to F3DZEX2.06H.NoN.fifo
 ID_STR := RSP Gfx ucode F3DZEX      fifo 2.06H Yoshitaka Yasumoto 1998 Nintendo.
 OPTIONS := \
   CFG_G_BRANCH_W \
@@ -238,7 +238,7 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DZEX_NoN_2.06H
+NAME := gspF3DZEX2.06H.NoN.fifo
 DESCRIPTION := Ocarina of Time
 ID_STR := RSP Gfx ucode F3DZEX.NoN  fifo 2.06H Yoshitaka Yasumoto 1998 Nintendo.
 MD5_CODE := 96a1a7a8eab45e0882aab9e4d8ccbcc3
@@ -251,8 +251,8 @@ OPTIONS := \
   BUG_FAIL_IF_CARRY_SET_AT_INIT
 $(eval $(call ucode_rule))
 
-NAME := F3DZEX_2.08I
-DESCRIPTION := Not in retail games; nearclipping added to F3DZEX_NoN_2.08I
+NAME := gspF3DZEX2.08I.fifo
+DESCRIPTION := Not in retail games; nearclipping added to F3DZEX2.08I.NoN.fifo
 ID_STR := RSP Gfx ucode F3DZEX      fifo 2.08I Yoshitaka Yasumoto/Kawasedo 1999.
 OPTIONS := \
   CFG_G_BRANCH_W \
@@ -260,7 +260,7 @@ OPTIONS := \
   BUG_WRONG_INIT_VZERO
 $(eval $(call ucode_rule))
 
-NAME := F3DZEX_NoN_2.08I
+NAME := gspF3DZEX2.08I.NoN.fifo
 DESCRIPTION := Majora's Mask
 ID_STR := RSP Gfx ucode F3DZEX.NoN  fifo 2.08I Yoshitaka Yasumoto/Kawasedo 1999.
 MD5_CODE := ca0a31df36dbeda69f09e9850e68c7f7
@@ -272,15 +272,15 @@ OPTIONS := \
   BUG_WRONG_INIT_VZERO
 $(eval $(call ucode_rule))
 
-NAME := F3DZEX_2.08J
-DESCRIPTION := Not in retail games; nearclipping added to F3DZEX_NoN_2.08J
+NAME := gspF3DZEX2.08J.fifo
+DESCRIPTION := Not in retail games; nearclipping added to F3DZEX2.08J.NoN.fifo
 ID_STR := RSP Gfx ucode F3DZEX      fifo 2.08J Yoshitaka Yasumoto/Kawasedo 1999.
 OPTIONS := \
   CFG_G_BRANCH_W \
   CFG_POINT_LIGHTING
 $(eval $(call ucode_rule))
 
-NAME := F3DZEX_NoN_2.08J
+NAME := gspF3DZEX2.08J.NoN.fifo
 DESCRIPTION := Animal Forest
 ID_STR := RSP Gfx ucode F3DZEX.NoN  fifo 2.08J Yoshitaka Yasumoto/Kawasedo 1999.
 MD5_CODE := a7f45433a67950cdd239ee40f1dd36c1
